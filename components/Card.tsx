63a6c00b3e1ce5ae70709cca8a569aab111d4196
imlouislab <louisamorosbessede@gmail.com>
2023-02-13 20:21:36 +0100

    card component with user colods

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,36 +1,71 @@
+import { useState } from "react";
 import { Database } from "../types/supabase";
 
-function ProjectItem(
-  cardInformation: Database["public"]["Tables"]["cards"]["Row"]
-) {
+function Card({
+  cardInformation,
+  portfolioData,
+}: {
+  cardInformation: Database["public"]["Tables"]["cards"]["Row"];
+  portfolioData: Database["public"]["Tables"]["portfolios"]["Row"];
+}) {
+  const initialColor =
+    portfolioData.card_color === null || portfolioData.card_color === ""
+      ? "black"
+      : portfolioData.card_color;
+  const [color, setColor] = useState(initialColor);
+
+  const hoveredColor =
+    portfolioData.card_color_hover === null ||
+    portfolioData.card_color_hover === ""
+      ? "gray"
+      : portfolioData.card_color_hover;
+
   return (
-    <div className="bg-slate-300 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-700 rounded-lg overflow-hidden hover:-translate-y-2 transform h-[330px] transition mt-5 mx-5">
+    <div
+      className="mx-5 mt-5 h-[330px] transform overflow-hidden rounded-lg transition hover:-translate-y-2"
+      style={{
+        color: portfolioData.background_color ?? "white",
+        backgroundColor: color ?? color,
+      }}
+      onMouseEnter={() => setColor(hoveredColor)}
+      onMouseLeave={() => setColor(initialColor)}
+    >
       <a href={"/"}>
         <img
           src={cardInformation.image ?? ""}
           alt={cardInformation.title ?? ""}
-          className="w-full h-36 md:h-48 object-cover"
+          className="h-36 w-full object-cover md:h-48"
         />
-        <div className="text-gray-600 dark:text-gray-300 p-5 w-full">
-          <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">
+        <div className="w-full p-5">
+          <h3
+            className="mb-2 text-lg font-semibold md:mb-3 md:text-xl"
+            style={{ color: portfolioData.text_minor_color ?? "white" }}
+          >
             {cardInformation.title}
           </h3>
-          <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
+          <p className="mb-2 text-sm md:mb-3 md:text-base">
+            {cardInformation.description}
+          </p>
+          <div className="flex flex-row flex-wrap items-center justify-start gap-2 text-xs md:text-sm">
             {cardInformation.keywords &&
               Array.isArray(cardInformation.keywords) &&
               cardInformation.keywords.map((keyword, index) => (
                 <span
                   key={index}
-                  className="inline-block px-2 py-1 bg-slate-200 dark:bg-slate-900 rounded-md"
+                  className="inline-block rounded-md px-2 py-1"
+                  style={{
+                    backgroundColor: portfolioData.background_color ?? "white",
+                    color: portfolioData.text_major_color ?? "black",
+                  }}
                 >
                   {keyword as string}
                 </span>
               ))}
-          </p>
+          </div>
         </div>
       </a>
     </div>
   );
 }
 
-export default ProjectItem;
+export default Card;
