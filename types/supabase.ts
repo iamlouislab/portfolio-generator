beea431a38c13ace0eecfa9a853adcdbc9a806f1
imlouislab <louisamorosbessede@gmail.com>
2023-02-13 20:22:08 +0100

    update db types

--- types/supabase.ts
+++ types/supabase.ts
@@ -43,22 +43,43 @@ export interface Database {
       }
       portfolios: {
         Row: {
-          backgroundColor: string | null
+          background_color: string | null
+          button_color: string | null
+          button_color_hover: string | null
+          card_color: string | null
+          card_color_hover: string | null
           created_at: string | null
           id: number
-          text: string | null
+          text_major_color: string | null
+          text_minor_2_color: string | null
+          text_minor_color: string | null
+          user: number | null
         }
         Insert: {
-          backgroundColor?: string | null
+          background_color?: string | null
+          button_color?: string | null
+          button_color_hover?: string | null
+          card_color?: string | null
+          card_color_hover?: string | null
           created_at?: string | null
           id?: number
-          text?: string | null
+          text_major_color?: string | null
+          text_minor_2_color?: string | null
+          text_minor_color?: string | null
+          user?: number | null
         }
         Update: {
-          backgroundColor?: string | null
+          background_color?: string | null
+          button_color?: string | null
+          button_color_hover?: string | null
+          card_color?: string | null
+          card_color_hover?: string | null
           created_at?: string | null
           id?: number
-          text?: string | null
+          text_major_color?: string | null
+          text_minor_2_color?: string | null
+          text_minor_color?: string | null
+          user?: number | null
         }
       }
       sections: {
@@ -90,20 +111,26 @@ export interface Database {
       users: {
         Row: {
           created_at: string | null
+          description: string | null
           id: number
           portfolio: number | null
+          profile_picture: string | null
           username: string | null
         }
         Insert: {
           created_at?: string | null
+          description?: string | null
           id?: number
           portfolio?: number | null
+          profile_picture?: string | null
           username?: string | null
         }
         Update: {
           created_at?: string | null
+          description?: string | null
           id?: number
           portfolio?: number | null
+          profile_picture?: string | null
           username?: string | null
         }
       }
