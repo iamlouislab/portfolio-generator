01403f214635e7dcd1c5169c6a88a278ef3d9074
imlouislab <louisamorosbessede@gmail.com>
2023-02-15 23:23:21 +0100

    display user portfolio

--- pages/profile/[username].tsx
+++ pages/profile/[username].tsx
@@ -4,6 +4,7 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { Database } from "../../types/supabase";
 import SectionComponent from "@/components/Section";
 import UserHelloSection from "@/components/UserHelloSection";
+import { Loading } from "@/components/Loading";
 
 function Username() {
   const router = useRouter();
@@ -22,6 +23,9 @@ function Username() {
     Array<Database["public"]["Tables"]["sections"]["Row"]>
   >([]);
 
+  const [unknownUser, setUnknownUser] = useState(false);
+  const [noPortfolio, setNoPortfolio] = useState(false);
+
   const loadData = async () => {
     const { data, error } = await supabase
       .from("users")
@@ -29,16 +33,21 @@ function Username() {
       .eq("username", username)
       .single();
     if (error) {
+      // No user found
       console.log(error);
+      setUnknownUser(true);
+      setLoadingComplete(true);
     } else {
       setUserData(data as Database["public"]["Tables"]["users"]["Row"]);
       const portfolioData = await supabase
         .from("portfolios")
         .select()
-        .eq("user", data.id)
+        .eq("user_id", data.user_id)
         .single();
       if (portfolioData.error) {
         console.log(error);
+        setNoPortfolio(true);
+        setLoadingComplete(true);
       } else {
         setPortfolio(
           portfolioData.data as Database["public"]["Tables"]["portfolios"]["Row"]
@@ -69,31 +78,46 @@ function Username() {
   }, [router.query, router.isReady, userData, portfolio]);
 
   if (username === undefined || !loadingComplete) {
-    return <div>Loading...</div>;
-  } else {
     return (
-      <div style={{ backgroundColor: portfolio.background_color ?? "black" }}>
-        <div className="mx-auto w-5/6">
-          <UserHelloSection userData={userData} portfolioData={portfolio} />
-          {userSections?.map(
-            (
-              section: Database["public"]["Tables"]["sections"]["Row"],
-              index
-            ) => (
-              <div key={index}>
-                <SectionComponent
-                  key={index}
-                  sectionInformation={
-                    section as Database["public"]["Tables"]["sections"]["Row"]
-                  }
-                  portfolioData={portfolio}
-                />
-              </div>
-            )
-          )}
-        </div>
+      <div className="flex min-h-screen items-center justify-center">
+        <Loading />
       </div>
     );
+  } else {
+    if (unknownUser) {
+      return <div>Unknown user</div>;
+    } else {
+      if (noPortfolio) {
+        return <div>User has no portfolio</div>;
+      } else {
+        return (
+          <div
+            style={{ backgroundColor: portfolio.background_color ?? "white" }}
+            className="min-h-screen"
+          >
+            <div className="mx-auto w-5/6">
+              <UserHelloSection userData={userData} portfolioData={portfolio} />
+              {userSections?.map(
+                (
+                  section: Database["public"]["Tables"]["sections"]["Row"],
+                  index
+                ) => (
+                  <div key={index}>
+                    <SectionComponent
+                      key={index}
+                      sectionInformation={
+                        section as Database["public"]["Tables"]["sections"]["Row"]
+                      }
+                      portfolioData={portfolio}
+                    />
+                  </div>
+                )
+              )}
+            </div>
+          </div>
+        );
+      }
+    }
   }
 }
 
