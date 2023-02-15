f66ea747bef9ae7c315d0c942a7a0d4abf5e97a8
imlouislab <louisamorosbessede@gmail.com>
2023-02-15 23:23:47 +0100

    search and redirect

--- components/SearchProfile.tsx
+++ components/SearchProfile.tsx
@@ -1,11 +1,28 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 export function SearchProfile() {
+  const [search, setSearch] = useState<string>("");
+  const router = useRouter();
   return (
     <div className="flex w-full max-w-sm items-center space-x-2">
-      <Input type="Profile" placeholder="Username" />
-      <Button type="submit">Search</Button>
+      <Input
+        type="Profile"
+        placeholder="Username"
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      <Button
+        type="submit"
+        onClick={() => {
+          router.push(`/profile/${search}`);
+        }}
+      >
+        Search
+      </Button>
     </div>
   );
 }
