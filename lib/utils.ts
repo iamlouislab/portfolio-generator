3801b58572a1480119a81cefbf35771156681413
imlouislab <louisamorosbessede@gmail.com>
2023-02-17 21:02:24 +0100

    baseColors

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const baseColors = {
+  background_color: "white",
+  text_major_color: "black",
+  text_minor_2_color: "gray",
+  text_minor_color: "white"
+}
