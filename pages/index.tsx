c183ea3cc90cfa0663d85c8b7172d38bde3842a7
imlouislab <louisamorosbessede@gmail.com>
2023-02-17 21:03:51 +0100

    landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,28 @@
-import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import React from "react";
+import ExampleImage from "@/public/example.png";
+import PageIllustration from "@/components/PageIllustrations";
+import Footer from "@/components/Footer";
+import FeaturesBlocks from "@/components/Features";
+import Testimonials from "@/components/Testimonials";
+import HeroHome from "@/components/HeroHome";
 
 const App = () => {
-  const supabaseClient = createBrowserSupabaseClient();
-
   return (
-    <section className="min-h-[600px] text-center bg-gray-400">
-      <p> Desgin beautiful portfolios in seconds</p>
-    </section>
+    <div className="flex min-h-screen flex-col overflow-hidden">
+      <main className="grow">
+        <div
+          className="pointer-events-none relative mx-auto h-0 max-w-6xl"
+          aria-hidden="true"
+        >
+          <PageIllustration />
+        </div>
+        <HeroHome />
+        <FeaturesBlocks />
+        <Testimonials />
+      </main>
+
+      <Footer />
+    </div>
   );
 };
-
 export default App;
