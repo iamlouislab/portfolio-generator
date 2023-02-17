1ff4357a6dee15c8d25e8146cb59054a7ad833cc
imlouislab <louisamorosbessede@gmail.com>
2023-02-17 21:01:52 +0100

    features landing page

---
+++ components/Features.tsx
@@ -0,0 +1,160 @@
+import React from "react";
+
+function FeaturesBlocks() {
+  return (
+    <section>
+      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+        <div className="py-12 md:py-20">
+          {/* Section header */}
+          <div className="mx-auto max-w-3xl pb-12 text-center md:pb-20">
+            <h2 className="h2 mb-4">
+              The majority our customers want to be able to display their skills
+              in a simple way.
+            </h2>
+          </div>
+
+          {/* Items */}
+          <div
+            className="mx-auto grid max-w-sm items-start gap-8 md:max-w-2xl md:grid-cols-2 lg:max-w-none lg:grid-cols-3 lg:gap-16"
+            data-aos-id-blocks
+          >
+            {/* 1st item */}
+            <div
+              className="relative flex flex-col items-center"
+              data-aos="fade-up"
+              data-aos-anchor="[data-aos-id-blocks]"
+            >
+              <svg
+                className="mb-4 h-16 w-16"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  className="fill-current text-purple-600"
+                  width="64"
+                  height="64"
+                  rx="32"
+                />
+                <path
+                  className="stroke-current text-purple-100"
+                  d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924"
+                  strokeLinecap="square"
+                  strokeWidth="2"
+                  fill="none"
+                  fillRule="evenodd"
+                />
+                <path
+                  className="stroke-current text-purple-300"
+                  d="M43 42h-9M43 37h-9"
+                  strokeLinecap="square"
+                  strokeWidth="2"
+                />
+              </svg>
+              <h4 className="h4 mb-2">Easy showcase</h4>
+              <p className="text-center text-lg text-gray-400">
+                Use our simple interface to showcase your skills and projects to
+                ensure you get the job using our intuitive interface to add
+                content to your portfolio.
+              </p>
+            </div>
+
+            {/* 2nd item */}
+            <div
+              className="relative flex flex-col items-center"
+              data-aos="fade-up"
+              data-aos-delay="100"
+              data-aos-anchor="[data-aos-id-blocks]"
+            >
+              <svg
+                className="mb-4 h-16 w-16"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  className="fill-current text-purple-600"
+                  cx="32"
+                  cy="32"
+                  r="32"
+                />
+                <path
+                  className="stroke-current text-purple-100"
+                  strokeWidth="2"
+                  strokeLinecap="square"
+                  d="M21 23h22v18H21z"
+                  fill="none"
+                  fillRule="evenodd"
+                />
+                <path
+                  className="stroke-current text-purple-300"
+                  d="M26 28h12M26 32h12M26 36h5"
+                  strokeWidth="2"
+                  strokeLinecap="square"
+                />
+              </svg>
+              <h4 className="h4 mb-2">Easy editing</h4>
+              <p className="text-center text-lg text-gray-400">
+                New projects? No problem. You can easily add new projects to
+                your portfolio. You can also fully customize the look and feel
+                of your portfolio using the colors of your choice.
+              </p>
+            </div>
+
+            {/* 3rd item */}
+            <div
+              className="relative flex flex-col items-center"
+              data-aos="fade-up"
+              data-aos-delay="200"
+              data-aos-anchor="[data-aos-id-blocks]"
+            >
+              <svg
+                className="mb-4 h-16 w-16"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  className="fill-current text-purple-600"
+                  width="64"
+                  height="64"
+                  rx="32"
+                />
+                <g
+                  transform="translate(21 21)"
+                  strokeLinecap="square"
+                  strokeWidth="2"
+                  fill="none"
+                  fillRule="evenodd"
+                >
+                  <ellipse
+                    className="stroke-current text-purple-300"
+                    cx="11"
+                    cy="11"
+                    rx="5.5"
+                    ry="11"
+                  />
+                  <path
+                    className="stroke-current text-purple-100"
+                    d="M11 0v22M0 11h22"
+                  />
+                  <circle
+                    className="stroke-current text-purple-100"
+                    cx="11"
+                    cy="11"
+                    r="11"
+                  />
+                </g>
+              </svg>
+              <h4 className="h4 mb-2">Easy hosting</h4>
+              <p className="text-center text-lg text-gray-400">
+                Your profile is hosted on our servers, so you don't need to
+                worry about hosting. Hurry up and get your profile online using
+                the username you want!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default FeaturesBlocks;
